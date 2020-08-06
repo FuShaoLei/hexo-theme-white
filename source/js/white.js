6e4460e1020eff69618b60ac2b75073f852df95d
FuShaoLei <1563250958@qq.com>
2020-08-06 22:47:50 +0800

    图片模式持续开发中

--- source/js/white.js
+++ source/js/white.js
@@ -70,3 +70,48 @@ sortdiv.onmouseout=function(){
 }
 }
 }
+
+//图片懒加载
+var imgs = document.querySelectorAll('img');
+var imgdivs = document.querySelectorAll('.img-show-post-cover');
+
+function hasClass( elements,cName ){
+return !!elements.className.match( new RegExp( "(\\s|^)" + cName + "(\\s|$)") ); // ( \\s|^ ) 判断前面是否有空格 （\\s | $ ）判断后面是否有空格 两个感叹号为转换为布尔值 以方便做判断
+};
+
+function addClass( elements,cName ){
+if( !hasClass(elements,cName ) ){
+    setTimeout(function() { elements.className += " " + cName; }, 500);
+};
+};
+
+
+//用来判断bound.top<=clientHeight的函数，返回一个bool值
+function isIn(el) {
+    var bound = el.getBoundingClientRect();
+    var clientHeight = window.innerHeight;
+    return bound.top <= clientHeight;
+} 
+//检查图片是否在可视区内，如果在，则加载
+function check() {
+    Array.from(imgs).forEach(function(el){
+        if(isIn(el)){
+            loadImg(el);
+        }
+    });
+    Array.from(imgdivs).forEach(function(el){
+        if(isIn(el)){     
+            addClass(el,"img-masks");
+        }
+    });
+}
+function loadImg(el) {
+    if(!el.src){
+        var source = el.dataset.src;
+        el.src = source;
+    }
+}
+
+window.onload = window.onscroll = function () { //onscroll()在滚动条滚动的时候触发
+    check();
+}
