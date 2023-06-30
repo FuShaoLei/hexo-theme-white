c288389d73f7719fe9d7963f4bfbe5ac83ef4138
FuShaoLei <1563250958@qq.com>
2023-07-01 00:27:15 +0800

    修复menu active item显示异常的问题，稍微调整了下引用的样式

--- source/js/white.js
+++ source/js/white.js
@@ -1,44 +1,35 @@
-//下面这一段全部来自于hexo-theme-nexmoe的部分源码，感谢大佬，指路: https://github.com/theme-nexmoe/hexo-theme-nexmoe
-const getRealPath = (pathname, desc = false) => {
-    if (!pathname) {
-        pathname = window.location.pathname;
-    }
-    let names = pathname.split("/");
-    if (desc === false) {
-        for (let i = names.length - 1; i >= 0; --i) {
-            let name = names[i].trim();
-            if (name.length > 0 && name !== "/" && name !== "index.html") {
-                return name;
-            }
-        }
-    } else {
-        for (let i = 0; i < names.length; ++i) {
-            let name = names[i].trim();
-            if (name.length > 0 && name !== "/" && name !== "index.html") {
-                return name;
-            }
-        }
-    }
-    return "/";
-};
+
+function isActive(pathname){
+    console.log("9988 pathname = "+pathname)
+
+    let rootPath = window.location.pathname
+    console.log("9988 rootPath = "+rootPath)
+
+    let path = pathname + "/"
+
+    return pathname===rootPath || path === rootPath;
+}
+
+
 
 let links = document.querySelectorAll(".nav-link");
-let rootRealPath = getRealPath(window.location.pathname, true);
+console.log(links)
 for (let link of links) {
     let linkPath = link.getAttribute("href");
-    if (linkPath && getRealPath(linkPath, true) === rootRealPath) {
+
+    if (isActive(linkPath)) {
         link.className = "active-link nav-link";
     }
+
 }
-// mobile-nav-link
 let mlinks = document.querySelectorAll(".mobile-nav-link");
-let mrootRealPath = getRealPath(window.location.pathname, true);
 for (let mlink of mlinks) {
     let mlinkPath = mlink.getAttribute("href");
-    if (mlinkPath && getRealPath(mlinkPath, true) === mrootRealPath) {
+    if (isActive(mlinkPath)) {
         mlink.className = "moibile-active-link mobile-nav-link";
     }
 }
+
 
 //设置收起的menu点击的效果
 var mmenu = document.getElementById("mobile-menu");
