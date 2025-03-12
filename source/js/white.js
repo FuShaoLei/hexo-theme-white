6a3b21c1a073f6ab817aa07bd6e7c971b913d4d8
fushaolei <1563250958@qq.com>
2025-03-12 16:38:23 +0800

    revert 先去除图片懒加载

--- source/js/white.js
+++ source/js/white.js
@@ -1,3 +1,6 @@
+/**
+ * Determine whether it is the current path
+ */
 function isActive(pathname) {
     let rootPath = window.location.pathname
     let path = pathname + "/"
@@ -90,45 +93,44 @@ if (document.getElementById("sort")) {
     }
 }
 
-// 图片懒加载
-var imgs = document.querySelectorAll('img.lazyload-img');
-var imgdivs = document.querySelectorAll('.lazyload-img-span');
-
-
-// 用来判断bound.top<=clientHeight的函数，返回一个bool值
-function isIn(el) {
-    var bound = el.getBoundingClientRect();
-    var clientHeight = window.innerHeight;
-    return bound.top <= clientHeight;
-}
-
-// 检查图片是否在可视区内，如果在，则加载
-function check() {
-    for (let p = 0; p < imgs.length; p++) {
-        if (isIn(imgs[p])) {
-            loadImg(imgs[p]);
-            imgs[p].onload = function () {
-                let y = Number(p);
-                changeClass(y);
-            }
-        }
-    }
-}
-
-function changeClass(num) {
-    console.log("changeClass(num) num = "+num)
-    let tempse = Number(num);
-    imgdivs[tempse].className = "lazyload-img-span img-masks";
-}
-
-function loadImg(el) {
-    console.log("loadImg el.dataset.src = "+el.dataset.src)
-    if (!el.src) {
-        el.src = el.dataset.src;
-    }
-}
-
-// onscroll()在滚动条滚动的时候触发
-window.onload = window.onscroll = function () {
-    check();
-}
+// var imgs = document.querySelectorAll('img.lazyload-img');
+// var imgdivs = document.querySelectorAll('.lazyload-img-span');
+//
+//
+// // 用来判断bound.top<=clientHeight的函数，返回一个bool值
+// function isIn(el) {
+//     var bound = el.getBoundingClientRect();
+//     var clientHeight = window.innerHeight;
+//     return bound.top <= clientHeight;
+// }
+//
+// // 检查图片是否在可视区内，如果在，则加载
+// function check() {
+//     for (let p = 0; p < imgs.length; p++) {
+//         if (isIn(imgs[p])) {
+//             loadImg(imgs[p]);
+//             imgs[p].onload = function () {
+//                 let y = Number(p);
+//                 changeClass(y);
+//             }
+//         }
+//     }
+// }
+//
+// function changeClass(num) {
+//     console.log("changeClass(num) num = "+num)
+//     let tempse = Number(num);
+//     imgdivs[tempse].className = "lazyload-img-span img-masks";
+// }
+//
+// function loadImg(el) {
+//     console.log("loadImg el.dataset.src = "+el.dataset.src)
+//     if (!el.src) {
+//         el.src = el.dataset.src;
+//     }
+// }
+//
+// // onscroll()在滚动条滚动的时候触发
+// window.onload = window.onscroll = function () {
+//     check();
+// }
